64c112db29f42e0c9eb465c570f0f7a1fba3eb7d
echoDaveD <dylong.david@googlemail.com>
2025-06-07 23:56:28 +0200

    sdf

--- custom_components/ehs_sentinel/__init__.py
+++ custom_components/ehs_sentinel/__init__.py
@@ -1,4 +1,5 @@
 from .const import DOMAIN
+from homeassistant.helpers.discovery import async_load_platform
 from .start import start_ehs_sentinel
 import logging
 
@@ -7,5 +8,9 @@ from .start import start_ehs_sentinel
 
 async def async_setup_entry(hass, config_entry):
     hass.data.setdefault(DOMAIN, {})
+     # Plattform „sensor“ explizit laden
+    hass.async_create_task(
+        async_load_platform(hass, "sensor", DOMAIN, {}, config_entry)
+    )
     hass.async_create_task(start_ehs_sentinel(hass, config_entry.data))
     return True
