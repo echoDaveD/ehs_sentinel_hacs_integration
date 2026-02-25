e381918b91a41a618707546f274637169d02ce92
echoDaveD <dylong.david@googlemail.com>
2026-02-25 20:11:45 +0100

    fix round on dict error in auto mode for target FLowTemp

--- custom_components/ehs_sentinel/coordinator.py
+++ custom_components/ehs_sentinel/coordinator.py
@@ -137,7 +137,7 @@ class EHSSentinelCoordinator(DataUpdateCoordinator):
             name = "Samsung EHSSentinel",
             manufacturer = "echoDave",
             model = "EHS Sentinel",
-            sw_version = "1.1.1",
+            sw_version = "1.1.2",
         )
     
     def register_entity_adder(self, category, adder):
